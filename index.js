351627d427b5f97be4014b51529866634b2e7b15
imonraj27 <77281501+imonraj27@users.noreply.github.com>
2023-06-11 16:58:53 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,53 @@
+// THIS IS THE BACKEND SERVER
+// WILL MAINTAIN THE REALTIME CODES
+// AMONG CONNECTED SOCKETS WITHIN A 
+// GROUP DEFINED BY A GROUPID
+// IT WILL TAKE SOCKET.IO REQUESTS FROM
+// OUR FRONTEND APPLICATION
+
+const express = require('express')
+const app = express();
+const http = require('http').createServer(app);
+const io = require("socket.io")(http, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+
+app.get('/', (req, res) => {
+    res.send("text")
+})
+
+
+io.on('connection', (socket) => {
+    console.log("someone joined");
+    socket.on('joinning', (groupid) => {
+        const groupSockets = io.sockets.adapter.rooms.get(groupid);
+        if (groupSockets) {
+            const socketsArray = Array.from(groupSockets);
+            const randomSocket = socketsArray[Math.floor(Math.random() * socketsArray.length)];
+            socket.join(groupid)
+            socket.groupid = groupid
+            console.log("here", socketsArray);
+
+            io.to(randomSocket).emit('supply');
+        } else {
+            console.log("shit");
+            socket.join(groupid)
+            socket.groupid = groupid
+        }
+
+    })
+
+    socket.on('freshdata', (text) => {
+        console.log(socket.groupid, socket.id, text);
+        socket.to(socket.groupid).emit('freshdata', text);
+    })
+
+})
+
+http.listen(4000, () => {
+    console.log("Server is Listening");
+})
