7bbe19109fbbd3c52485b6be373c238eccf0eb8d
imonraj27 <77281501+imonraj27@users.noreply.github.com>
2023-06-11 19:48:06 +0530

    ok

--- index.js
+++ index.js
@@ -8,12 +8,15 @@
 const express = require('express')
 const app = express();
 const http = require('http').createServer(app);
+const cors = require('cors')
 const io = require("socket.io")(http, {
     cors: {
         origin: ["http://localhost:3000", "https://realtime-colab.netlify.app/"],
         methods: ["GET", "POST"]
     }
 });
+
+app.use(cors());
 
 
 app.get('/', (req, res) => {
